caa3e71b951be0873fd5536d02b96649e500d5d0
saiful142348 <saiful17182103353@gmail.com>
2022-08-03 13:56:33 +0600

    friendrequest success

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,6 +58,6 @@ class User extends Authenticatable
 
     public function friends()
     {
-        return $this->hasMany(Friend::class);
+        return $this->hasMany(Friend::class)->latest();
     }
 }
--- resources/views/user/pages/friend_request.blade.php
+++ resources/views/user/pages/friend_request.blade.php
@@ -1,18 +1,56 @@
+{{-- @foreach ($item->friends as $fr)
+    
+@if ($fr->auth_id==Auth::user()->id)
+<form action="{{ url('user/unfriend/' . $fr->user_id) }}" method="post">
+    @method('DELETE')
+    @csrf
 
-@if (!$item->friends->isEmpty() )
- 
-        <form action="{{ url('user/unfriend/' . $item->friends[0]->user_id) }}" method="post">
-            @method('DELETE')
-            @csrf
+    <button class="btn-success"><i class=" fa fa-thumbs-up">Requested</i></button>
+</form>
+@else
+<form action="{{ route('user.friend') }}" method="post">
+    @csrf
 
-            <button class="btn-success"><i class=" fa fa-thumbs-up">Requested</i></button>
-        </form>
-    @else
-        <form action="{{ route('user.friend') }}" method="post">
-            @csrf
-            {{-- <input type="hidden" name="auth_id" value="{{Auth::user()->id}}"> --}}
-            <input type="hidden" name="user_id" value="{{ $item->id }}">
-            <input type="submit" value="friend add">
-        </form>
-    @endif
+    <input type="hidden" name="user_id" value="{{ $item->id }}">
+    <input type="submit" value="friend add">
+</form>
+
+@endif
+       
+
+
+      
+       
+        
+@endforeach --}}
+@if (!$item->friends->count()>0 )
+<form action="{{ route('user.friend') }}" method="post">
+    @csrf
+
+    <input type="hidden" name="user_id" value="{{ $item->id }}">
+    <input type="submit" value="friend add">
+</form> 
+@else 
+@if (Auth::user()->id==$item->friends->first()->auth_id && $item->friends->first()->friend==0)
+<form action="{{ url('user/unfriend/' . $item->friends[0]->user_id) }}" method="post">
+    @method('DELETE')
+    @csrf
+
+    <button class="btn-success"><i class=" fa fa-thumbs-up">Requested</i></button>
+</form>
+@else
+<form action="{{ route('user.friend') }}" method="post">
+    @csrf
+
+    <input type="hidden" name="user_id" value="{{ $item->id }}">
+    <input type="submit" value="friend add">
+</form> 
+@endif
+
+
+
+
+
+
+@endif
 
